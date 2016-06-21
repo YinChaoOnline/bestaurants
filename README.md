04ea12416647c1f223b064b31eb56b55ecf992cb
yinchaoonline <ychaowhu@foxmail.com>
2016-06-21 16:53:13 +0800

    add restaurants layer and use app/config file to store configuration of the app

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ best restaurants in Belize,a Learning Web GIS demo from the book--Building Web A
 1. use bootstrap to build UI.
 2. add Belize layer.
 3. add spatial and attribute query.
+4. add edit functionality.
---
+++ app/config.js
@@ -0,0 +1,69 @@
+define(['esri/InfoTemplate'], function (InfoTemplate) {
+
+    return {
+        mapOptions: {
+            extentJson: {
+                "xmin": 30236013.176644534,
+                "ymin": 1976510.339413753,
+                "xmax": 30272702.950221367,
+                "ymax": 1988568.2806257235,
+                "spatialReference": {
+                    "wkid": 102100
+                }
+            },
+            basemap: "osm"
+            //minZoom: 4,
+            //isScrollWheelZoom: true,
+            //nav: false,
+            //logo: false,
+            //wrapAround180: true,
+            //showLabels: true
+        },
+
+        /*
+         *operation layers,belize restaurants layer
+         */
+        layers: {
+            belizePointLayer: {
+                url: 'http://localhost:6080/arcgis/rest/services/BelizeEdit/MapServer',
+                title: 'Restaurants',
+                options: {
+                    id: 'Restaurants',
+                    opacity: 1.0,
+                    visible: false
+                }
+            }
+        },
+
+        // Add config parameters for each map widget you want to include
+        // The map reference will get appended to the options
+        // To accept default options just pass empty object {}
+        // NOTE: to change the position of these widgets, make changes in map.css
+        widgets: {
+            scalebar: {
+
+            },
+            homeButton: {
+
+            },
+            locateButton: {
+
+            },
+            layerList: {
+
+            },
+            bookMarks: {
+
+            },
+            geocoder: {
+                autoComplete: true,
+                arcgisGeocoder: {
+                    placeholder: 'Address or Location'
+                },
+                'class': 'geocoder'
+            }
+        },
+
+        portalUrl: 'http://www.arcgis.com'
+    };
+});
